e418665e395f855bd598db2e9a95f2b628faf823
Eduardo Rojas <eduardo.rojas@nagro.app>
2021-11-17 01:10:58 -0300

    up-dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,74 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <!--  -->
+      <v-list disabled>
+        <v-subheader>Dashboard</v-subheader>
+        <v-list-item-group v-model="selectedItem" color="primary">
+          <v-list-item v-for="(item, i) in items" :key="i" :class='item.classe'>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item-content>
+             <v-list-item-icon>
+              <v-icon v-text="item.icon" :class='item.classe'></v-icon>
+            </v-list-item-icon>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app id="barraNav">
+      <v-app-bar-nav-icon
+        @click="drawer = !drawer"
+        color="white"
+      ></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        <v-img
+          max-height="150"
+          max-width="250"
+          :src="require('../assets/img/logo.svg')"
+        ></v-img>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: ()=> {
+    return {
+      drawer: null,
+      items: [
+        { text: "Empresas", icon: "mdi-domain", classe: 'active cursorActive'},
+        { text: "Produtos", icon: "mdi-check", classe: 'cursorActive'},
+        { text: "Serviços", icon: "mdi-flag", classe: 'cursorActive' },
+      ],
+    }
+  },
+};
+</script>
+<style scoped>
+#barraNav {
+  background-color: #093677;
+}
+
+.active{
+  background-color: #0DB5B5;
+  color: white !important;
+}
+
+.cursorActive{
+  cursor: pointer;
+}
+
+.cursorActive:hover{
+  cursor: pointer;
+  background-color: #0DB5B5;
+  color: white !important;
+}
+</style>
